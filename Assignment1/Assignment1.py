8bb9104280cc6f8418dea47d5768224e663e2d86
Rients Dalstra <ri.dalstra@st.hanze.nl>
2022-05-11 17:28:46 +0200

    Finnished the assignment
    Kinda not really,
    Currently it is obtaining 1 reference from 10 random pubmed files.
    I can also get it to obtain 10 refrences from 1 pubmed file but then the multiprocessing isn't applied.

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -3,17 +3,10 @@ import Bio
 from Bio import Entrez
 from Bio import Medline
 import json
+import numpy as np
 
 Entrez.api_key = 'b73a5ffde89ba2ae4feca63960fdac659009'
 Entrez.email = 'rie123@live.nl'
-pmid = "24524923"
-
-def fetching(pmid):
-    handle = Entrez.efetch(db="pubmed", id=pmid, retmode="xml")
-    record = Entrez.read(handle)
-    handle.close()
-    return record
-
 
 def final_script(pmid):
     # input is an pubmed id
@@ -27,22 +20,25 @@ def final_script(pmid):
 
     for link in records:
         references.append(link['Id'])
+    k = np.random.randint(0,len(references) - 1)
+    k1 = k + 1
 
-    for item in references[1:]:
-        k = k + 1
-        if k <= 10:
-            record = fetching(item)
-            with open('output/'+ item + '.xml','w') as to_write:
-                to_write.write(json.dumps(record['PubmedArticle'][0]))
-            to_write.close()
-            print(k)
-        else:
-            print('Finnished')
-            break
+    for item in references[k:k1]:
+        handle = Entrez.efetch(db="pubmed", id=item, retmode="xml")
+        record = Entrez.read(handle)
+        handle.close()
+        with open('output/'+ item + '.xml','w') as to_write:
+            to_write.write(json.dumps(record['PubmedArticle'][0]))
+        to_write.close()
+        print('Finnished with 1 ref')
 
 
-def multi_process(pmid):
-    if __name__ == "__main__":
-        cpus = mp.cpu_count()
-        with mp.Pool(cpus) as pool:
-            results = pool.map(final_script, pmid)
+nr = "1234567891"
+def multi_process():
+    cpus = mp.cpu_count()
+    with mp.Pool(cpus) as pool:
+        results = pool.map(final_script, nr)
+
+        
+if __name__ == "__main__":       
+    multi_process()
