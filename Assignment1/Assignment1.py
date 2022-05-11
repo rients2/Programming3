9efd06c3933af95a1c6cf48a291ce2106cd75ea6
Rients Dalstra <ri.dalstra@st.hanze.nl>
2022-05-11 16:41:01 +0200

    Almost finished

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -2,25 +2,47 @@ import multiprocessing as mp
 import Bio
 from Bio import Entrez
 from Bio import Medline
+import json
 
 Entrez.api_key = 'b73a5ffde89ba2ae4feca63960fdac659009'
 Entrez.email = 'rie123@live.nl'
+pmid = "24524923"
+
+def fetching(pmid):
+    handle = Entrez.efetch(db="pubmed", id=pmid, retmode="xml")
+    record = Entrez.read(handle)
+    handle.close()
+    return record
 
 
-#handle = Entrez.efetch(db="pubmed", id=" 33525617", retmax=10)
-handle = Entrez.elink(dbfrom= 'pubmed', id="33537979", linkname="pubmed_pubmed")
+def final_script(pmid):
+    # input is an pubmed id
+    # output is 10 xml articles based on refrences i hope.
+    references = []
+    k = 0
 
-print(handle.readline().strip())
-# LOCUS       AY851612                 892 bp    DNA     linear   PLN 10-APR-2007
-handle.close()
+    links = Entrez.elink(dbfrom="pubmed", id=pmid, linkname="pubmed_pubmed")
+    record = Entrez.read(links)
+    records = record[0]['LinkSetDb'][0]['Link']
+
+    for link in records:
+        references.append(link['Id'])
+
+    for item in references[1:]:
+        k = k + 1
+        if k <= 10:
+            record = fetching(item)
+            with open('output/'+ item + '.xml','w') as to_write:
+                to_write.write(json.dumps(record['PubmedArticle'][0]))
+            to_write.close()
+            print(k)
+        else:
+            print('Finnished')
+            break
 
 
-# def func(uid="19304878"):
-#     pmid = uid
-#     handle = Entrez.elink(dbfrom="pubmed", id=pmid, linkname="pubmed_pubmed")
-#     record = Entrez.read(handle)
-#     handle.close()
-#     print(record[0]["LinkSetDb"][0]["LinkName"])
-#     pubmed_pubmed
-#     linked = [link["Id"] for link in record[0]["LinkSetDb"][0]["Link"]]
-#     "17121776" in linked
+def multi_process(pmid):
+    if __name__ == "__main__":
+        cpus = mp.cpu_count()
+        with mp.Pool(cpus) as pool:
+            results = pool.map(final_script, pmid)
