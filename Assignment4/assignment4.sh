f66a048824817dd5248d7919bc88df04f89434cb
Rients Dalstra <ri.dalstra@st.hanze.nl>
2022-06-08 17:52:31 +0200

    Added slurm commands

--- Assignment4/assignment4.sh
+++ Assignment4/assignment4.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+#SBATCH --time 48:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+
 velveth output 21 -fastq -short /data/dataprocessing/MinIONData/MG5267/MG5267_TGACCA_L008_R2_001_BC24EVACXX.filt.fastq
 velvetg output -exp_cov auto -cov_cutoff auto
