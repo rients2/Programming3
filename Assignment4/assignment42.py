00966683a847ea42fa1a822aea7b1706ae9dbc46
rients2 <ri.dalstra@st.hanze.nl>
2022-06-10 10:39:58 +0200

    Fixed duplicate kmar len bug

--- Assignment4/assignment42.py
+++ Assignment4/assignment42.py
@@ -2,13 +2,16 @@ import pandas as pd
 import shutil
 
 df = pd.read_csv('output/kmars.csv',header=None)
-df = df.drop_duplicates()
+df = df.drop_duplicates(subset=1, keep="last")
 df = df[df[1] == df[1].max()]
 
 kmar_len = int(df[0])
+print(kmar_len)
 
 og = f'/students/2021-2022/master/Rients_DSLS/output/{kmar_len}/contigs.fa'
 
 target = '/homes/rdalstra/Documents/Programming3/Assignment4/output/contigs.fa'
 
 shutil.move(og, target)
+
+print('Done')
